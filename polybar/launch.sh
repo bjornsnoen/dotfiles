8090a943a2299d408a63e29cceb15f0634236bc8
Bjørn Polat-Snoen <bjorn.snoen@gmail.com>
2023-03-27 19:06:45 +0200

    Some tray fix

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,15 +5,23 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar --list-monitors | while read -r m; do
-    MONITOR=$(echo $m | cut -d":" -f1)
-    PRIMARY=$(echo $m | grep primary)
+num_monitors=$(polybar --list-monitors | wc -l)
 
-    if [ -z "$PRIMARY" ]; then
-        TRAY="none"
-    else
-        TRAY="right"
-    fi
+polybar --list-monitors | while read -r m; do
+    case $num_monitors in
+        1)
+            TRAY="right"
+            ;;
+        *)
+            MONITOR=$(echo $m | cut -d":" -f1)
+            PRIMARY=$(echo $m | grep primary)
+            if [ -z "$PRIMARY" ]; then
+                TRAY="none"
+            else
+                TRAY="right"
+            fi
+            ;;
+    esac
 
     TRAY=$TRAY MONITOR=$MONITOR polybar --reload arch &
 done
