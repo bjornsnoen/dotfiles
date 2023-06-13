a559e3ac0fd08f87cfdf8fbf03ab7667e41e6dbe
Bjørn Polat-Snoen <bjorn@fink.no>
2023-06-13 15:36:30 +0200

    Separate wayland dotfiles installer, lots of fixes

---
+++ hypr/cleanup_after_start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sleep 10
+hyprctl keyword windowrule "workspace unset,kitty"
+hyprctl keyword windowrule "workspace unset,evolution"
+hyprctl keyword windowrule "workspace unset,firefox"
