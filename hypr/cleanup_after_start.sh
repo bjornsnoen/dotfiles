40129350d7abb7e92c620a20ab0e41107d15a5d8
Bjørn Polat-Snoen <bjorn@brbcoffee.com>
2023-09-06 16:05:55 +0100

    Good wayland config

--- hypr/cleanup_after_start.sh
+++ hypr/cleanup_after_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sleep 10
+sleep 3
 hyprctl keyword windowrule "workspace unset,kitty"
 hyprctl keyword windowrule "workspace unset,evolution"
-hyprctl keyword windowrule "workspace unset,firefox"
+hyprctl keyword windowrule "workspace unset,brave"
