e726549f15cc070983ddbca67c69a01a03295c50
Bjørn Polat-Snoen <bjorn@snoen.io>
2024-10-09 16:54:14 +0100

    Update hyprland config

--- hypr/cleanup_after_start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sleep 3
-hyprctl keyword windowrule "workspace unset,kitty"
-hyprctl keyword windowrule "workspace unset,evolution"
-hyprctl keyword windowrule "workspace unset,firefox"
